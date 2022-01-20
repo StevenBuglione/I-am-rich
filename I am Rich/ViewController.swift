287dbe8621865982f04eb66669ffc21ed53f5930
Steven Buglione <stevenbuglione@Stevens-Mini.fios-router.home>
2022-01-19 19:19:09 -0500

    Added First IOS app

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Steven Buglione on 1/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
